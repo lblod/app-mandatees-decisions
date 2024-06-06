5df45715853429290b623df6eb3239e326421d9b
felix <ruiz.felixruiz@gmail.com>
2024-06-06 14:08:23 +0200

    Flush mu-resource cache

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -18,6 +18,21 @@ export default [
 
   {
     match: {
+      subject: {},
+    },
+    callback: {
+      url: "http://resource/.mu/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true,
+    },
+  },
+
+  {
+    match: {
     },
     callback: {
       url: 'http://delta-producer-publication-graph-maintainer/delta',
