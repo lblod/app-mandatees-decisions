024997e849463dd0322553d1be1a2367a6fd1ea3
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-11-19 10:06:18 +0100

    lmb-1053: set time to consume from closer to now + remove resource and modified service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -43,21 +43,4 @@ export default [
       retryTimeout: 250,
     },
   },
-
-  {
-    match: {
-      subject: {},
-    },
-    callback: {
-      url: 'http://modified/delta',
-      method: 'POST',
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 10000,
-      retry: 3,
-      ignoreFromSelf: true,
-      retryTimeout: 250,
-    },
-  },
 ];
