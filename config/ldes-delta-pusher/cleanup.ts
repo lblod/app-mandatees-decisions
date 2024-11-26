d084139f0866b944bf402a429e1bf694bc4527b5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-11-26 09:18:43 +0100

    lmb-1053: add cron to cleanup the public graph

---
+++ config/ldes-delta-pusher/cleanup.ts
@@ -0,0 +1,39 @@
+import { querySudo } from "@lblod/mu-auth-sudo";
+import { CronJob } from 'cron';
+
+import { log } from "./logger";
+
+const CLEANUP_CRON = process.CRON_PATTERN_CLEANUP || '0 */2 * * *'; // Fallback is every 2 hours
+
+new CronJob(CLEANUP_CRON, async () => {
+  console.log(`[***************************************************]`);
+  console.log(` Cleaning up the public graph`);
+  console.log(`[***************************************************]`);
+
+  const interestingTypes = [
+    "http://data.vlaanderen.be/ns/besluit#Artikel",
+    "http://data.vlaanderen.be/ns/besluit#Besluit"
+  ];
+  const escapedInterestingTypes = interestingTypes.map((type) => `<${type}>`)
+    .join(", ")
+
+  log(
+    `Removing all non interesting subjects from graph: http://mu.semte.ch/graphs/public`,
+    "debug"
+  );
+
+  await querySudo(`
+    DELETE {
+      GRAPH <http://mu.semte.ch/graphs/public> {
+        ?s ?p ?o.
+      }
+    }
+    WHERE {
+      GRAPH <http://mu.semte.ch/graphs/public> {
+        ?s a ?type.
+        ?s ?p ?o.
+      }
+      FILTER (?s NOT IN ( ${escapedInterestingTypes} ) )
+    }
+  `)
+}).start();
